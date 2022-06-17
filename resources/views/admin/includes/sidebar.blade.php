da53833c238af142218d2c7e7ab8978dacec01f6
aziz.hassan <aziz.hassan@nxb.com.pk>
2022-06-17 16:50:12 +0500

    added sidebar active link

--- resources/views/admin/includes/sidebar.blade.php
+++ resources/views/admin/includes/sidebar.blade.php
@@ -1,3 +1,4 @@
+{{--@dd( Request::segment(2)  )--}}
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="{{ url('admin/dashboard') }}" class="brand-link text-center">
@@ -35,7 +36,7 @@
                 <!-- Add icons to the links using the .nav-icon class
                      with font-awesome or any other icon font library -->
                 <li class="nav-item">
-                    <a href="{{ url('admin/dashboard') }}" class="nav-link">
+                    <a href="{{ url('admin/dashboard') }}" class="nav-link {{ Request::segment(2) === 'dashboard' ? 'active' : null }}">
                         <i class="nav-icon fas fa-tachometer-alt"></i>
                         <p>
                             Dashboard
@@ -43,7 +44,7 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a href="{{ url('admin/survey-stats') }}" class="nav-link">
+                    <a href="{{ url('admin/survey-stats') }}" class="nav-link {{ Request::segment(2) === 'survey-stats' ? 'active' : null }}">
                         <i class="nav-icon fas fa-user-alt"></i>
                         <p>
                             Survey Responses
@@ -52,7 +53,7 @@
                 </li>
 
                 <li class="nav-item">
-                    <a href="{{ url('admin/video-stats') }}" class="nav-link">
+                    <a href="{{ url('admin/video-stats') }}" class="nav-link {{ Request::segment(2) === 'video-stats' ? 'active' : null }}">
                         <i class="nav-icon fas fa-user-alt"></i>
                         <p>
                             Video Playing Stats
