ae2903fe8fe6a2588e202a8b4a7a710dc1a0e520
aziz.hassan <aziz.hassan@nxb.com.pk>
2022-07-07 18:46:46 +0500

    modified the question seeder

--- database/seeders/QuestionsSeeder.php
+++ database/seeders/QuestionsSeeder.php
@@ -20,10 +20,10 @@ class QuestionsSeeder extends Seeder
             'question' => [
                 'question' => 'What is your most preferred time for vacation?',
                 'options' => [
-                    'A' => 'Jan-Mar',
-                    'B' => 'Apr-Jun',
-                    'C' => 'Jul-Sep',
-                    'D' => 'Oct-Dec',
+                    'Jan-Mar',
+                    'Apr-Jun',
+                    'Jul-Sep',
+                    'Oct-Dec',
                 ]
             ]
         ]);
@@ -33,13 +33,13 @@ class QuestionsSeeder extends Seeder
             'question' => [
                 'question' => 'What is your preferred destination?',
                 'options' => [
-                    'A' => 'United states',
-                    'B' => 'Americas',
-                    'C' => 'Europe',
-                    'D' => 'Australia',
-                    'E' => 'Europe',
-                    'F' => 'Asia',
-                    'G' => 'Africa'
+                    'United states',
+                    'Americas',
+                    'Europe',
+                    'Australia',
+                    'Europe',
+                    'Asia',
+                    'Africa'
                 ]
             ],
         ]);
@@ -49,10 +49,10 @@ class QuestionsSeeder extends Seeder
             'question' => [
                 'question' => 'What is your planned budget for vacation?',
                 'options' => [
-                    'A' => '<$5k',
-                    'B' => '$5k-$10k',
-                    'C' => '$10k-$20k',
-                    'D' => '>$20k',
+                    '<$5k',
+                    '$5k-$10k',
+                    '$10k-$20k',
+                    '>$20k',
                 ]
             ],
 
@@ -63,10 +63,10 @@ class QuestionsSeeder extends Seeder
             'question' => [
                 'question' => 'How soon are you planning a vacation?',
                 'options' => [
-                    'A' => 'Within a month',
-                    'B' => '1-3 months',
-                    'C' => '3-6 months',
-                    'D' => '~ 6 months'
+                    'Within a month',
+                    '1-3 months',
+                    '3-6 months',
+                    '~ 6 months'
                 ]
             ]
         ]);
@@ -77,10 +77,10 @@ class QuestionsSeeder extends Seeder
             'question' => [
                 'question' => 'Where do you prefer to have your breakfast?',
                 'options' => [
-                    'A' => 'At home',
-                    'B' => 'In my transport',
-                    'C' => 'At work',
-                    'D' => 'I don’t like breakfast'
+                    'At home',
+                    'In my transport',
+                    'At work',
+                    'I don’t like breakfast'
                 ]
             ]
         ]);
@@ -90,14 +90,14 @@ class QuestionsSeeder extends Seeder
             'question' => [
                 'question' => 'On average how many days do you eat your breakfast?',
                 'options' => [
-                    'A' => '0',
-                    'B' => '1',
-                    'C' => '2',
-                    'D' => '3',
-                    'E' => '4',
-                    'F' => '5',
-                    'G' => '6',
-                    'H' => '7'
+                    '0',
+                    '1',
+                    '2',
+                    '3',
+                    '4',
+                    '5',
+                    '6',
+                    '7'
                 ]
             ]
         ]);
@@ -107,9 +107,9 @@ class QuestionsSeeder extends Seeder
             'question' => [
                 'question' => 'How often do you buy breakfast at Breakfast restaurants (McDonald\'s, Dunkin Donuts, etc)?',
                 'options' => [
-                    'A' => 'Once a week',
-                    'B' => '2-3 days a week',
-                    'C' => 'more than 3 days a week',
+                    'Once a week',
+                    '2-3 days a week',
+                    'more than 3 days a week',
                 ]
             ]
         ]);
@@ -120,14 +120,14 @@ class QuestionsSeeder extends Seeder
             'question' => [
                 'question' => 'How often do you use public transportation in a week?',
                 'options' => [
-                    'A' => '0',
-                    'B' => '1',
-                    'C' => '2',
-                    'D' => '3',
-                    'E' => '4',
-                    'F' => '5',
-                    'G' => '6',
-                    'H' => '7'
+                    '0',
+                    '1',
+                    '2',
+                    '3',
+                    '4',
+                    '5',
+                    '6',
+                    '7'
                 ]
             ]
         ]);
@@ -137,14 +137,14 @@ class QuestionsSeeder extends Seeder
             'question' => [
                 'question' => 'How often do you use ride sharing services (Uber, OLA, etc) in a week?',
                 'options' => [
-                    'A' => '0',
-                    'B' => '1',
-                    'C' => '2',
-                    'D' => '3',
-                    'E' => '4',
-                    'F' => '5',
-                    'G' => '6',
-                    'H' => '7'
+                    '0',
+                    '1',
+                    '2',
+                    '3',
+                    '4',
+                    '5',
+                    '6',
+                    '7'
                 ]
             ]
         ]);
@@ -154,9 +154,9 @@ class QuestionsSeeder extends Seeder
             'question' => [
                 'question' => 'On average, how many miles do you travel using Ride sharing, Public transportation in a week?',
                 'options' => [
-                    'A' => '0-100 miles',
-                    'B' => '100-250 miles',
-                    'C' => 'Greater than 250 miles'
+                    '0-100 miles',
+                    '100-250 miles',
+                    'Greater than 250 miles'
                 ]
             ]
         ]);
